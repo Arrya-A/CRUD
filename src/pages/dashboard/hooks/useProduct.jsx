38b0ad70420347080138d7720b18dea22a566a68
arrya <arrya@bpract.com>
2025-05-28 17:50:37 +0530

    v

--- src/pages/dashboard/hooks/useProduct.jsx
+++ src/pages/dashboard/hooks/useProduct.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import axiosInstance from "../../../utils/axiosInstance";
 const useProduct = () => {
-  return (
-    <div>useProduct</div>
-  )
-}
+  const [allProducts, setAllProducts] = useState([]);
 
-export default useProduct
+  //API to fetch all the products
+  const fetchProducts = async () => {
+    try {
+      const response = await axiosInstance.get("allProducts");
+      console.log(response.data);
+      setAllProducts(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //API to add new products
+  const addProduct = async (newProductData) => {
+    try {
+      const response = await axiosInstance.post("allProducts", newProductData);
+      console.log(response.data);
+      
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return { allProducts, fetchProducts, addProduct };
+};
+
+export default useProduct;
